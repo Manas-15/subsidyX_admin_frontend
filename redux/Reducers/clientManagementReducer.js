3a9c15a49b390828956ec0711f08e2c98e835a02
Manas Pasayat <andolasoft.user187@gmail.com>
2023-07-13 12:14:54 +0530

    modified reducer for fetch client

--- redux/Reducers/clientManagementReducer.js
+++ redux/Reducers/clientManagementReducer.js
@@ -1,28 +1,40 @@
 import { clientData } from "../../static/clientData";
 import { clientManagementConstants } from "../Constants/clientManagementConstants";
 
-export function client(
-    state = { isSuccess: false, clients: [] },
-    action
-) {
-    switch (action.type) {
+export function client(state = { isSuccess: false, clients: [] }, action) {
+  switch (action.type) {
+    case clientManagementConstants.GET_CLIENT_SUCCESS:
+      return {
+        ...state,
+        isSuccess: true,
+        clients: clientData,
+      };
 
-        case clientManagementConstants.CREATE_CLIENT_SUCCESS:
-            console.log(action);
-            return { ...state, isSuccess: true, clients: [...state.clients, action.data.body] };
-        case clientManagementConstants.UPDATE_CLIENT_SUCCESS:
-            return {
-                ...state, isSuccess: true, clients: state.clients.map(c => {
-                    if (+c.id === +action.data.id) {
-                        return action.data.editData
-                    }
-                    return c;
-                })
-            };
-        case clientManagementConstants.DELETE_CLIENT_SUCCESS:
-            return { ...state, isSuccess: true, clients: state.clients.filter(c => c.id !== action.data.id) };
+    case clientManagementConstants.CREATE_CLIENT_SUCCESS:
+      return {
+        ...state,
+        isSuccess: true,
+        clients: [...state.clients, action.data.body],
+      };
+    case clientManagementConstants.UPDATE_CLIENT_SUCCESS:
+      return {
+        ...state,
+        isSuccess: true,
+        clients: state.clients.map((c) => {
+          if (+c.id === +action.data.id) {
+            return action.data.editData;
+          }
+          return c;
+        }),
+      };
+    case clientManagementConstants.DELETE_CLIENT_SUCCESS:
+      return {
+        ...state,
+        isSuccess: true,
+        clients: state.clients.filter((c) => c.id !== action.data.id),
+      };
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 }
